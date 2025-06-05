1ec054db0aef2371cfcefc74bbcb9a62a70ddd09
Fiumara-Facundo <facundofiumara@gmail.com>
2025-06-04 21:03:29 -0300

    commit 3

--- src/modelo/zonas/Stand.java
+++ src/modelo/zonas/Stand.java
@@ -1,19 +1,27 @@
 package modelo.zonas;
 
-public class Stand {
-    private String nombre;
-    private int numero;
+import modelo.personas.Comerciante;
+import modelo.enums.TipoZona;
+
+import java.util.*;
+
+public class Stand extends ZonaRestringida{
+    private String ubicacion;
+    private Comerciante responsable;
+    private List<String> empleados= new ArrayList<>();
 
 
-    public Stand(String nombre, int numero){
-        this.nombre=nombre;
-        this.numero=numero;
+    public Stand(String codigo, String descripcion, int capacidadMaxima, String ubicacion, Comerciante responsable) {
+        super(codigo, descripcion, capacidadMaxima);
+        this.tipo = TipoZona.STAND;
+        this.ubicacion = ubicacion;
+        this.responsable = responsable;
     }
 
-    @Override
-    public String toString(){
-        return this.nombre +"  "+ this.numero;
-    }
+  public void agregarEmpleado(String nombre){empleados.add(nombre);}
+  public String getUbicacion(){return ubicacion;}
+    public Comerciante getResponsable() { return responsable; }
+    public List<String> getEmpleados() { return empleados; }
 
 
 
