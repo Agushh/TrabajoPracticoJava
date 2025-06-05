1b4aac8165cacc2c00f83f0c1814ea0804872c33
Fiumara-Facundo <facundofiumara@gmail.com>
2025-06-05 10:25:49 -0300

    Opcion para ahorrar switch en TipoZona

--- src/modelo/enums/TipoZona.java
+++ src/modelo/enums/TipoZona.java
@@ -14,3 +14,30 @@ public enum TipoZona {
     }
 }
 
+/*
+Comentario facu: lo mismo que con tipopers, se puede ahorrar el switch y creo que queda mas prolijo:
+
+package modelo.enums;
+
+public enum TipoZona {
+    ESCENARIO("ESC"),
+    ZONA_COMUN("COM"),
+    ZONA_RESTRINGIDA("RES"),
+    STAND("STA");
+
+    private final String codigo;
+
+    TipoZona(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return codigo;
+    }
+}
+
+
+*/
+
+
